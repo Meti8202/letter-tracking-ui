307f3c0be14a47421fb6233a3fe2f849b8c04439
MetiGM <silvie8202@gmail.com>
2024-04-05 21:31:05 +0300

    Implement Header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import {
+  Menu,
+  Link,
+  Package2,
+  Home,
+  ShoppingCart,
+  Badge,
+  Package,
+  Users,
+  LineChart,
+  Search,
+  CircleUser,
+  Mailbox,
+  PencilLine,
+  TableProperties,
+} from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const currentPath = usePathname();
+
+  return (
+    <div className="flex flex-col">
+      <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[80px] lg:px-6">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="flex flex-col">
+            <nav className="grid gap-2 text-lg font-medium">
+              <Link
+                href="/"
+                className="flex items-center gap-2 h-14 font-semibold"
+              >
+                <Mailbox className="h-6 w-6" />
+                <span className="font-Tac_One text-3xl">Letter Tracking</span>
+              </Link>
+              <Link
+                href="/"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Home className="h-5 w-5" />
+                Dashboard
+              </Link>
+              <Link
+                href="./compose"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 max-w-xs transition duration-100 ease-in-out hover:scale-105 ${
+                  currentPath === "/compose" ? "bg-muted" : ""
+                }`}
+              >
+                <PencilLine className="h-4 w-4" />
+                Compose Letter
+              </Link>
+              <Link
+                href="./letterlist"
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 max-w-xs transition duration-100 ease-in-out hover:scale-105 ${
+                  currentPath === "/letterlist" ? "bg-muted" : ""
+                }`}
+              >
+                <TableProperties className="h-4 w-4" />
+                Letter List
+              </Link>
+              {/* <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Users className="h-5 w-5" />
+                Customers
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <LineChart className="h-5 w-5" />
+                Analytics
+              </Link>*/}
+            </nav>
+          </SheetContent>
+        </Sheet>
+        {/* <div className="w-full flex-1">
+          <form>
+            <div className="relative">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search..."
+                className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+              />
+            </div>
+          </form>
+        </div> */}
+        <div className="absolute right-11">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-7 w-7" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </header>
+    </div>
+  );
+}
