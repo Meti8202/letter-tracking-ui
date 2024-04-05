2c71c7a565307751023bd292163afeb14e3a7adc
MetiGM <silvie8202@gmail.com>
2024-04-05 21:30:15 +0300

    Implement Dashboard layout component
    - Added a Dashboard component responsible for the layout of the application.
    - Incorporated Sidebar and Header components to provide navigation and header functionality.

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+// Dashboard.tsx
+import React from "react";
+import Sidebar from "@/components/sidebar/Sidebar";
+import Header from "@/components/header/Header";
+
+export default function Dashboard({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <Sidebar />
+      <div className="flex flex-col w-full">
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
+}
