6634c6835ef1a43de460b782fe1b3a15514d7685
MetiGM <silvie8202@gmail.com>
2024-04-05 21:22:55 +0300

    Create Home page component
    Implemented the Home page component to display various cards with icons and text content. Utilized Lucide React icons for visual representation. Used the Card, CardHeader, CardTitle, and CardContent components from "@/components/ui/card" for consistent styling.

---
+++ src/app/page.tsx
@@ -0,0 +1,64 @@
+// page.tsx
+import React from "react";
+import {
+  DollarSign,
+  Users,
+  CreditCard,
+  Activity,
+  HashIcon,
+} from "lucide-react";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <div className="flex-1">
+      <main className="flex flex-col gap-4 p-4 md:gap-8 md:p-8">
+        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">dui ut</CardTitle>
+              <HashIcon className="h-4 w-4 text-muted-foreground" />{" "}
+              {/*Hash icon */}
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">arcu odio</div>
+              <p className="text-xs text-muted-foreground">
+                aliquam etiam erat velit scelerisque
+              </p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium"></CardTitle>
+              <Users className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold"></div>
+              <p className="text-xs text-muted-foreground"></p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium"></CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold"></div>
+              <p className="text-xs text-muted-foreground"></p>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium"></CardTitle>
+              <Activity className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold"></div>
+              <p className="text-xs text-muted-foreground"></p>
+            </CardContent>
+          </Card>
+        </div>
+      </main>
+    </div>
+  );
+}
