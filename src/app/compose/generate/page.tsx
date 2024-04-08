4f2b46c3f499b0fda9b7a8f3509f385055a1d135
MetiGM <silvie8202@gmail.com>
2024-04-08 18:04:17 +0300

    Setup components needed to generate the letter

--- src/app/compose/generate/page.tsx
+++ src/app/compose/generate/page.tsx
@@ -1,3 +1,33 @@
-export default function GenerateLtter() {
-  return <div>This is the page that users use to generate their letter.</div>;
+/* eslint-disable react/jsx-no-undef */
+"use client";
+
+import ClosingSalutation from "@/components/LetterTemplate/ClosingSalutation";
+import DateComponent from "@/components/LetterTemplate/Date";
+import LetterBody from "@/components/LetterTemplate/LetterBody";
+import RecipientContactAddress from "@/components/LetterTemplate/RecipientAddress";
+import Salutation from "@/components/LetterTemplate/Salutation";
+import SenderContactAddress from "@/components/LetterTemplate/SenderAddress";
+import Signature from "@/components/LetterTemplate/Signature";
+import SubjectLine from "@/components/LetterTemplate/SubjectLine";
+
+export default function GenerateLetter() {
+  return (
+    <>
+      <div>
+        <h2 className="text-2xl font-semibold mb-5 mt-11 ml-8 mr-6">
+          Compose Letter
+        </h2>
+        <div className="grid gap-1 m-11">
+          <DateComponent />
+          <RecipientContactAddress />
+          <SubjectLine />
+          <Salutation />
+          <LetterBody />
+          <ClosingSalutation />
+          <Signature />
+          <SenderContactAddress />
+        </div>
+      </div>
+    </>
+  );
 }
